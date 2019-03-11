a3a05b6415dc9e6c51585e02caa301b0fff96c81
Ricardo Calixto <rcalixtos@gmail.com>
2019-03-10 22:53:55 -0300

    store-reducer

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = () => (
+  <ul>
+    <li>Acordar cedo</li>
+    <li>Fazer café</li>
+    <li>Terminar módulo de ReactJS</li>
+    <li>Terminar aula de flexbox</li>
+  </ul>
+);
+
+export default TodoList;
---
+++ src/store/reducers/todos.js
@@ -0,0 +1,3 @@
+export default function todos(state, action) {
+  // state é como o estado está antes da alteração
+}
