89b325974c519e1feeb79f56b9bdfe404af8bbd0
Ricardo Calixto <rcalixtos@gmail.com>
2019-03-11 10:01:58 -0300

    add and remove todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map(todo => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
+const TodoList = ({ todos, addTodo, removeTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.text}
+          <button type="submit" onClick={() => removeTodo(todo.id)}>
+            Remove
+          </button>
+        </li>
+      ))}
+    </ul>
+    <button type="submit" onClick={() => addTodo('sex')}>
+      Add
+    </button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
@@ -24,4 +38,9 @@ const mapStateToProps = state => ({
   todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -9,6 +9,12 @@ export default function todos(state = INITIAL_STATE, action) {
   // action é a action que foi disparada pela aplicação
   // no reducer há uma manipulação do acction.type para saber qual action que o reducer vai alterar
   switch (action.type) {
+    case 'ADD_TODO': {
+      return [...state, { id: Math.random(), text: action.payload.text }];
+    }
+    case 'REMOVE_TODO': {
+      return state.filter(todo => todo.id !== action.payload.id);
+    }
     default:
       return state;
   }
